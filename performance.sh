d40e89903601edd0722ab870ee36a5d0180879ed
roxerg <roxguda@gmail.com>
2023-12-08 01:55:19 +0000

    Comment in utility script

--- performance.sh
+++ performance.sh
@@ -3,7 +3,8 @@
 for day in $(ls -d -- */) 
 do 
     cd $day
-    TIMEFORMAT="${day%/}: %R s"
+    TIMEFORMAT="${day%/}: time: %E s"
+    # uses fishshell implementation of time
     time python3 "${day%/}.py" &>/dev/null
     cd ../
 done
